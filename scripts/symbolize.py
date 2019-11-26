jforissier/optee_os

commit 105e09c24479903c90e2f086fdefd540950fc728

symbolize.py: add support for TEE core ASLR
With the introduction of ASLR, the abort and panic dumps for the TEE
core have gained a "Load address" line. Update the symbolize.py script
to parse that line and pass relative addresses to addr2line.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Jerome Forissier <jerome@forissier.org>

scripts/symbolize.py
@@ -15,6 +15,7 @@ import sys
 import termios
 
 CALL_STACK_RE = re.compile('Call stack:')
+TEE_LOAD_ADDR_RE = re.compile(r'Load address @ (?P<load_addr>0x[0-9a-f]+)')
 # This gets the address from lines looking like this:
 # E/TC:0  0x001044a8
 STACK_ADDR_RE = re.compile(
@@ -161,7 +162,11 @@ class Symbolizer(object):
         cmd = self.arch_prefix('addr2line')
         if not cmd:
             return
-        self._addr2line = self.my_Popen([cmd, '-f', '-p', '-e', elf])
+        args = [cmd]
+        if elf_name == 'tee.elf' and self._tee_load_addr != '0x0':
+            args += ['-j.text']
+        args += ['-f', '-p', '-e', elf]
+        self._addr2line = self.my_Popen(args)
         self._addr2line_elf_name = elf_name
 
     # If addr falls into a region that maps a TA ELF file, return the load
@@ -184,14 +189,14 @@ class Symbolizer(object):
             return '0x0'
         else:
             # tee.elf
-            return '0x0'
+            return self._tee_load_addr
 
     def elf_for_addr(self, addr):
+        if not self._regions:
+            return 'tee.elf'
         l_addr = self.elf_load_addr(addr)
         if l_addr is None:
             return None
-        if l_addr is '0x0':
-            return 'tee.elf'
         for k in self._elfs:
             e = self._elfs[k]
             if int(e[1], 16) == int(l_addr, 16):
@@ -364,6 +369,7 @@ class Symbolizer(object):
         self._sections = {}  # {elf_name: [[name, addr, size], ...], ...}
         self._regions = []   # [[addr, size, elf_idx, saved line], ...]
         self._elfs = {0: ["tee.elf", 0]}  # {idx: [uuid, load_addr], ...}
+        self._tee_load_addr = 0x0
         self._func_graph_found = False
         self._func_graph_skip_line = True
 
@@ -426,6 +432,9 @@ class Symbolizer(object):
             self._elfs[i] = [match.group('uuid'), match.group('load_addr'),
                              line]
             return
+        match = re.search(TEE_LOAD_ADDR_RE, line)
+        if match:
+            self._tee_load_addr = match.group('load_addr')
         match = re.search(CALL_STACK_RE, line)
         if match:
             self._call_stack_found = True

commit f9a10643eb2dec0b0f72f67ac8d7b4321d9280cd

symbolize.py: fix decoding of ftrace dumps containing syscalls
When decoding an ftrace file with syscall tracing enabled [1], the
kernel functions are not resolved and show question marks instead.

[1] $ make CFG_FTRACE_SUPPORT=y CFG_SYSCALL_FTRACE=y CFG_ULIBS_MCOUNT=y \
           CFLAGS_ta_arm32=-pg
    [run test and copy content of /tmp/ftrace*]
    $ optee_os/scripts/symbolize.py -d optee_os/out/arm/core \
           -d out-br/build/optee_test-1.0/ta/*/out
    [paste ftrace log here]

Fixes: 105e09c24479 ("symbolize.py: add support for TEE core ASLR")
Signed-off-by: Jerome Forissier <jerome@forissier.org>

scripts/symbolize.py
@@ -186,17 +186,15 @@ class Symbolizer(object):
             # especially to symbolize mixed (user-space and kernel) addresses
             # which is true when syscall ftrace is enabled along with TA
             # ftrace.
-            return '0x0'
+            return self._tee_load_addr
         else:
             # tee.elf
             return self._tee_load_addr
 
     def elf_for_addr(self, addr):
-        if not self._regions:
-            return 'tee.elf'
         l_addr = self.elf_load_addr(addr)
-        if l_addr is None:
-            return None
+        if l_addr == self._tee_load_addr:
+            return 'tee.elf'
         for k in self._elfs:
             e = self._elfs[k]
             if int(e[1], 16) == int(l_addr, 16):
@@ -369,7 +367,7 @@ class Symbolizer(object):
         self._sections = {}  # {elf_name: [[name, addr, size], ...], ...}
         self._regions = []   # [[addr, size, elf_idx, saved line], ...]
         self._elfs = {0: ["tee.elf", 0]}  # {idx: [uuid, load_addr], ...}
-        self._tee_load_addr = 0x0
+        self._tee_load_addr = '0x0'
         self._func_graph_found = False
         self._func_graph_skip_line = True
 