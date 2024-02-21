e2f75ddcbfc2d5ef068c9f69d7dbd50b45a5514a
Alvin Chang <alvinga@andestech.com>
2024-02-21 14:18:00 +0800

    libutils: Import part of sys/fcntl.h
    Import part of sys/fcntl.h and sys/_default_fcntl.h for file flags, from
    newlib repository:
    - newlib/newlib/libc/include/sys/fcntl.h
    
    Signed-off-by: Alvin Chang <alvinga@andestech.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

---
+++ lib/libutils/isoc/include/sys/fcntl.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 1994-2009  Red Hat, Inc. All rights reserved.
+ */
+
+#ifndef _SYS_FCNTL_H_
+#define _SYS_FCNTL_H_
+#include <sys/_default_fcntl.h>
+#endif /* _SYS_FCNTL_H_ */
